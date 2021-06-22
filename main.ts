0c1fa3a87b1f92eb2099aa8feb114cf576904afc
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-06-23 00:25:20 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,32 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    controller = receivedNumber
+radio.onReceivedString(function (receivedString) {
+    controller = receivedString
 })
-let controller = 0
+let controller = ""
+let turn_speed = 0
 radio.setGroup(1)
-controller = 0
+let max_speed = 110
+let min_speed = 70
 basic.forever(function () {
-    if (controller > 0) {
-        pins.servoWritePin(AnalogPin.P12, Math.map(controller, 0, 100, 90, 180))
-        pins.servoWritePin(AnalogPin.P16, Math.map(100 - controller, 0, 100, 90, 180))
-    } else if (controller < 0) {
-        pins.servoWritePin(AnalogPin.P16, Math.map(Math.abs(controller), -100, 0, -180, 90))
-        pins.servoWritePin(AnalogPin.P12, Math.map(100 - Math.abs(controller), -100, 0, -180, 90))
+    if (controller == "stop") {
+        pins.servoWritePin(AnalogPin.P12, 90)
+        pins.servoWritePin(AnalogPin.P16, 90)
+    } else if (controller == "forward") {
+        pins.servoWritePin(AnalogPin.P12, max_speed)
+        pins.servoWritePin(AnalogPin.P16, min_speed)
+    } else if (controller == "back") {
+        pins.servoWritePin(AnalogPin.P12, min_speed)
+        pins.servoWritePin(AnalogPin.P16, max_speed)
+    } else if (controller == "right") {
+        pins.servoWritePin(AnalogPin.P12, max_speed)
+        pins.servoWritePin(AnalogPin.P16, max_speed)
+    } else if (controller == "left") {
+        pins.servoWritePin(AnalogPin.P12, min_speed)
+        pins.servoWritePin(AnalogPin.P16, min_speed)
     } else {
         pins.servoWritePin(AnalogPin.P12, 90)
         pins.servoWritePin(AnalogPin.P16, 90)
     }
+})
+basic.forever(function () {
+    basic.showString(controller)
 })
